c0f1d49c2f7db806aca0baac2dfed783048fbbb9
alamin mahamud <alamin.ineedahelp@gmail.com>
2018-10-05 15:25:43 -0600

    add: forms

--- examples/forms/js/index.js
+++ examples/forms/js/index.js
@@ -1,14 +1,14 @@
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: 'Please write an essay about your favourite DOM element.'};
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
     this.setState({
-      value: event.target.value
+      value: event.target.value.toUpperCase()
     });
   }
 
@@ -20,12 +20,14 @@ class NameForm extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>Name:</label>
-        <input
-          type="text"
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
+
+        <label>
+          Essay:
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange} />
+        </label>
+
         <input type="submit" value="Submit" />
       </form>
     );
