9045e3017e4061b88c93b5b738bc0478de35ac8b
alamin mahamud <alamin.ineedahelp@gmail.com>
2018-10-08 14:34:53 +0600

    temp commit
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- examples/react-crash-course/counter-app/src/components/counter.jsx
+++ examples/react-crash-course/counter-app/src/components/counter.jsx
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
-  formatCount(){
-    const { count } = this.state;
-    return (count === 0) ? 'Zero' : count;
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      tags: ['tag1', 'tag2', 'tag3']
+    };
+    this.getBadgeClasses = this.getBadgeClasses.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
+
 
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-small">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleClick}
+          className="btn btn-secondary btn-small">Increment</button>
+        <ul>
+          {this.renderTags()}
+        </ul>
       </div>
     );
+  }
+
+  handleClick() {
+    this.setState({
+      value: this.state.value + 1
+    })
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0 ) return "<p>There are no Tags! </p>";
+    return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
+  }
+
+  formatCount() {
+      const { value } = this.state;
+      return (value === 0) ? 'Zero' : value;
+    }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
   }
 }
 
