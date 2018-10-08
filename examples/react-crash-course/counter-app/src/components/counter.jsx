7c7607444aafce927cbbd2223defdd772c23cf2d
alamin mahamud <alamin.ineedahelp@gmail.com>
2018-10-08 16:21:30 +0600

    :beers: counter-app

--- examples/react-crash-course/counter-app/src/components/counter.jsx
+++ examples/react-crash-course/counter-app/src/components/counter.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 
+
 class Counter extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: 0,
-      tags: ['tag1', 'tag2', 'tag3']
-    };
     this.getBadgeClasses = this.getBadgeClasses.bind(this);
-    this.handleClick = this.handleClick.bind(this);
   }
 
 
@@ -17,34 +13,23 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleClick}
-          className="btn btn-secondary btn-small">Increment</button>
-        <ul>
-          {this.renderTags()}
-        </ul>
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-small">
+          Increment
+        </button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
-
-  handleClick() {
-    this.setState({
-      value: this.state.value + 1
-    })
-  }
-
-  renderTags() {
-    if (this.state.tags.length === 0 ) return "<p>There are no Tags! </p>";
-    return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
-  }
-
+  
   formatCount() {
-      const { value } = this.state;
+      const { value } = this.props.counter;
       return (value === 0) ? 'Zero' : value;
-    }
+  }
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    classes += (this.props.counter.value === 0) ? 'warning' : 'primary';
     return classes;
   }
 }
