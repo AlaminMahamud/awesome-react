46130bd83bf42d9f144cf861b5d7afcde61d75d2
alamin mahamud <alamin.ineedahelp@gmail.com>
2018-10-10 17:16:45 +0600

    :beers: notebook mockup

--- examples/notebook/src/App.js
+++ examples/notebook/src/App.js
@@ -1,24 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-            title: 'React Starter',
-            description: 'A basic template that consists of the essential elements that are required to start building a React application'
-    };
-  }
 
-  render() {
-    return (
-            <div>
-                <Header />
-                <div className="container mt-5">
-                    <NoteManager />
-                </div>
-            </div>
-        );
-  }
-}
+const Home = () => (
+  <h1>Hello World</h1>
+);
+
+const About = () => (
+  <h1>Hello About</h1>
+);
+
+const Notes = ({ match }) => (
+  <div>
+    <h2>Notes</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:noteId`} component={Note} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a Note.</h3>}
+    />
+  </div>
+);
+
+const Note = ({ match }) => (
+  <div>
+    <h3>Hello #{match.params.noteId}</h3>
+  </div>
+);
+
+
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/notes">Notes</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/notes" component={Notes} />
+    </div>
+
+
+  </BrowserRouter>
+);
 
 export default App;
